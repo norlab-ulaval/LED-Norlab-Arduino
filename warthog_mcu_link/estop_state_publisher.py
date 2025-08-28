a27285d1bdb6e8d4b7c105deadab04bd14838e24
Warthog low level <norlabwarthog@gmail.com>
2025-08-28 14:20:31 -0400

    Fusing the two ros serial nodes. The correct node is now estop_state_publisher should change the name but a pull request on norlab robot is necessary  by [BR]

--- warthog_mcu_link/estop_state_publisher.py
+++ warthog_mcu_link/estop_state_publisher.py
@@ -1,61 +1,71 @@
-#| /usr/bin/env python3
+#!/usr/bin/env python3
 
 import rclpy
+from rclpy.node import Node
 
 from clearpath_platform_msgs.msg import Lights
 from std_msgs.msg import Bool
 
-from rclpy.node import Node
-
 import serial
 
-import time
-
-class estop_pub(Node):
-
+class WarthogArduinoInterface(Node):
 
     def __init__(self):
+        super().__init__('warthog_arduino_interface')
 
-        super().__init__('estate_state_publisher')
-        
-        self.arduino = serial.Serial()
-        self.arduino.port = '/dev/ttyUSB0'
-        self.arduino.baudrate = 115200
-        self.arduino.timeout = 2
-        self.arduino.open()
-        #self.initialise_link()
+        # Serial connection setup
+        self.arduino = serial.Serial(
+            port='/dev/ttyUSB0',
+            baudrate=115200,
+            timeout=2
+        )
+
         self.estop_bool = True
-        self.publisher_ = self.create_publisher(Bool, '/warthog/platform/emergency_stop', 10)
-        self.timer = self.create_timer(0.01, self.estop_state_publisher)
         self.cntr = 0
-    
+
+        self.estop_publisher = self.create_publisher(Bool, '/warthog/platform/emergency_stop', 10)
+
+        self.lights_subscription = self.create_subscription(
+            Lights,
+            '/cmd_lights',
+            self.lights_callback,
+            10
+        )
+
+        # Timer for reading estop state
+        self.estop_timer = self.create_timer(0.01, self.estop_state_publisher)
+
+    def lights_callback(self, msg):
+        data = []
+        for item in msg.lights:
+            data.extend([item.red, item.green, item.blue])
+
+        log = ','.join([str(int(d * 8)) for d in data]) + "\n"
+        self.arduino.write(log.encode('utf-8'))
+
     def estop_state_publisher(self):
         msg = Bool()
-
         line = self.arduino.readline().strip()
-            
-        if (line == b'1'):
+
+        if line == b'1':
             self.estop_bool = True
             self.cntr = 0
+        #Filtering the false readings
         else:
             self.cntr += 1
-            if (self.cntr == 5):
+            if self.cntr >= 5:
                 self.estop_bool = False
                 self.cntr = 0
-                
+
         msg.data = self.estop_bool
-        # self.get_logger().info(str(msg.data))
-        self.publisher_.publish(msg)
-
-
+        self.estop_publisher.publish(msg)
 
 def main():
     rclpy.init()
-    node = estop_pub()
+    node = WarthogArduinoInterface()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == "__main__":
-
     main()
